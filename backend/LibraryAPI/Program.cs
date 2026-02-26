8346dde390500531cff65414557d64394abc5a2f
Oshani Kavindya Halgamuwa <oshani.20221137@iit.ac.lk>
2026-02-26 21:47:16 +0530

    added sqlite and ef setup

--- backend/LibraryAPI/Program.cs
+++ backend/LibraryAPI/Program.cs
@@ -1,9 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using LibraryAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Services
 builder.Services.AddControllers();
 
+// SQLite + Entity Framework Core
+builder.Services.AddDbContext<LibraryContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
+// Auto-apply migrations on startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<LibraryContext>();
+    db.Database.Migrate();
+}
 
 app.UseHttpsRedirection();
 app.MapControllers();
