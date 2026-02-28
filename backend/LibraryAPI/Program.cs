52245f63bdc77a3c29d7a024b203053aba191092
Oshani Kavindya Halgamuwa <oshani.20221137@iit.ac.lk>
2026-02-28 08:20:01 +0530

    added auth0 login

--- backend/LibraryAPI/Program.cs
+++ backend/LibraryAPI/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using LibraryAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +13,22 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<LibraryContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Auth0 JWT Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}/";
+        options.Audience = builder.Configuration["Auth0:Audience"];
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = $"https://{builder.Configuration["Auth0:Domain"]}/",
+            ValidAudience = builder.Configuration["Auth0:Audience"]
+        };
+    });
+    
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
@@ -22,5 +41,7 @@ using (var scope = app.Services.CreateScope())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 app.Run();
--- backend/LibraryAPI/obj/Debug/net8.0/LibraryAPI.AssemblyInfo.cs
+++ backend/LibraryAPI/obj/Debug/net8.0/LibraryAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc9fb28e254271c2c55ae1b7ec008131645e3566")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e20005d0ba21362356c780753b70f774c94d3e22")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
